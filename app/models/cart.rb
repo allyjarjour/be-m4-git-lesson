55a746141bf51348799cb313b2b7beee50092731
Ally Jarjour <allyjarjour@gmail.com>
2020-07-01 10:04:15 -0600

    Delete formate_date method, add_item and create_order items methods

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,10 +5,6 @@ class Cart
     @contents = contents || {}
   end
 
-  def add_item(id)
-    contents[id.to_s] = (contents[id.to_s] || 0) + 1
-  end
-
   def cart_total_price
     contents.map { |key, value| Item.find(key).price * value }.sum
   end
